1575c09e9bf5b51688749c6f12ec1e340efd909a
Ryan <dsouza.ryan.11@gmail.com>
2020-10-13 23:30:10 +0530

    update tests

--- test/aws-cdk.test.ts
+++ test/aws-cdk.test.ts
@@ -1,13 +1,71 @@
 import { config } from 'dotenv';
 config();
 import * as cdk from '@aws-cdk/core';
-import { SynthUtils } from '@aws-cdk/assert';
+import * as Codebuild from '@aws-cdk/aws-codebuild';
+import { expect as expecCDK, haveResourceLike } from '@aws-cdk/assert';
 import { AwsCdkStack } from '../lib/aws-cdk-stack';
+import { envVars } from '../lib/config';
 
-const stackName = 'Node-EB-Deploy';
-
-test('snapshot works correctly', () => {
-  const app = new cdk.App();
-  const stack = new AwsCdkStack(app, stackName);
-  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+test('creates an Elastic Beanstalk app', () => {
+  const stack = createStack();
+  expecCDK(stack).to(
+    haveResourceLike('AWS::ElasticBeanstalk::Application', {
+      ApplicationName: 'node-eb',
+    })
+  );
 });
+
+test('creates an Elastic Beanstalk environment', () => {
+  const stack = createStack();
+  expecCDK(stack).to(
+    haveResourceLike('AWS::ElasticBeanstalk::Environment', {
+      ApplicationName: 'node-eb',
+      EnvironmentName: 'main',
+    })
+  );
+});
+
+test('creates a Codebuild project', () => {
+  const stack = createStack();
+  expecCDK(stack).to(
+    haveResourceLike('AWS::CodeBuild::Project', {
+      Artifacts: {
+        Type: 'NO_ARTIFACTS',
+      },
+      Environment: {
+        ComputeType: Codebuild.ComputeType.SMALL,
+        EnvironmentVariables: [
+          {
+            Name: 'EB_STAGE',
+            Type: 'PLAINTEXT',
+            Value: envVars.APP_STAGE_NAME,
+          },
+        ],
+        PrivilegedMode: false,
+      },
+      Name: 'node-eb-build',
+      TimeoutInMinutes: 20,
+      Triggers: {
+        FilterGroups: [
+          [
+            {
+              Pattern: 'PUSH, PULL_REQUEST_MERGED',
+              Type: 'EVENT',
+            },
+            {
+              Pattern: envVars.BUILD_BRANCH,
+              Type: 'HEAD_REF',
+            },
+          ],
+        ],
+        Webhook: true,
+      },
+    })
+  );
+});
+
+function createStack() {
+  const stackName = 'Node-EB-Deploy';
+  const app = new cdk.App();
+  return new AwsCdkStack(app, stackName);
+}
